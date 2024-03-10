68ee9991737b25cae8a88b495cd240e22564d37d
MercedesPoisson <mercedespoisson@yahoo.com>
2024-03-10 19:11:53 -0300

    getFavorites => ok && toggleFavorite => ok

---
+++ server/Controllers/toggleFavorite.js
@@ -0,0 +1,20 @@
+const Book = require("../models/book");
+
+const toggleFavorite = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const book = await Book.findById(id);
+        if(!book) {
+            return res.status(404).json({ message: "Libro no encontrado" });
+        }
+
+        const bookTitle = book.title;
+        book.isFavorite = !book.isFavorite;
+        await book.save()
+        res.status(200).json({ message: `El estado de favoritos ${bookTitle} se actualizó correctamente `});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = toggleFavorite;
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,14 @@ const postBook = require("../Controllers/postBook");
 const getBookById = require("../Controllers/getBookById");
 const putBook = require("../Controllers/putBook");
 const deleteBook = require("../Controllers/deleteBook");
+const getFavorites = require("../Controllers/getFavorites");
+const toggleFavorite = require("../Controllers/toggleFavorite");
 
 const router = Router();
+
+router.get("/fav", getFavorites);
+
+router.put("/fav/:id", toggleFavorite);
 
 router.get("/", getAllBooks);
 
@@ -16,6 +22,9 @@ router.get("/:id", getBookById);
 router.put("/:id", putBook);
 
 router.delete("/:id", deleteBook);
+
+
+
 
 router.all("*", (req, res) => {
   res.status(404).send("Ruta no encontrada");
